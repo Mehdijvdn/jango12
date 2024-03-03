1d7f837d8e0ec80c9301f0920fd151b02cbb2b13
mehdi <you@example.com>
2024-03-03 21:48:06 +0330

    email v3

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
@@ -37,6 +38,7 @@ def post_detail(request, year, month, day, post):
         'blog/post/detail.html',
         {'post': post})
 def post_share(request, post_id):
+    sent = False
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
     
@@ -47,7 +49,18 @@ def post_share(request, post_id):
         # Form fields passed validation
             cd = form.cleaned_data
         # ... send email
+        post_url = request.build_absolute_uri(
+            post.get_absolute_url())
+        subject = f"{cd['name']} recommends you read " \
+                    f"{post.title}"
+        message = f"Read {post.title} at {post_url}\n\n" \
+                    f"{cd['name']}\'s comments: {cd['comments']}"
+        send_mail(subject, message, 'admin@myblog.com',
+                    [cd['to']])
+        sent = True
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,
-                                                        'form': form})
+                                                    'form': form,
+                                                    'sent': sent})
+    
